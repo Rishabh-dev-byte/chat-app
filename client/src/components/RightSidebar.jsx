94bc8e9fc6dd30faed0e266cc173ec1af0057aa4
Rishabh Tiwari <rishabhtiwari3479@gmail.com>
2026-03-13 20:28:28 +0530

    feat: enhance RightSidebar with user profile display and add logout button; update LoginPage layout and form handling

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -3,7 +3,7 @@ import assets, { imagesDummyData } from '../assets/assets'
 
 const RightSidebar = ({selectedUser}) => {
   return  selectedUser &&(
-    <div className={`bg-[#8185B2]/10 text-white w-full relative overflow-y-scroll ${selectedUser ? "max-md:hidden" : ''}`}>
+    <div className={`bg-[#8185B2]/10 text-white w-full h-full relative overflow-y-scroll ${selectedUser ? "max-md:hidden" : ''}`}>
       <div className='flex flex-col pt-16 items-center font-light mx-auto gap-2 text-xs'>
         <img src={selectedUser?.profilePic || assets.avatar_icon} 
         className='w-20 aspect-[1/1] rounded-full'/>
@@ -11,22 +11,29 @@ const RightSidebar = ({selectedUser}) => {
           <p className='w-2 h-2 rounded-full bg-green-500'></p>
           {selectedUser.fullName}
         </h1>
-        <p1 className="px-10 mx-auto">{selectedUser.bio}</p1>
+        <p className="mx-auto">{selectedUser?.bio}</p>
       </div>
       <hr className='border-[#ffffff50] my-4'></hr>
-      <div className='px-5 text-xl'>
+      <div className='px-5 text-xs '>
         <p>Media</p>
-        <div className='mt-2 max-h-[200px] overflow-y-scroll grid grid-cols-2 gap-4 opacity-80'>
+        <div className='mt-2 max-h-200 overflow-y-scroll grid grid-cols-2 gap-4 opacity-80 mb-2'>
           {imagesDummyData.map((url,index)=>(
-            <div key={index} onClick={()=>{window.open(url)}} className='cursor-pointer rounded'>
-            <img src={url} className='h-full rounded-md'/>
-          
-
+            <div key={index} onClick={()=>{window.open(url)}} 
+            className='cursor-pointer rounded'>
+            <img src={url} className="h-full rounded-md" />
             </div>
           ))}
         </div>
       </div>
+      <div className='flex absolute bottom-5 left-1/2 transform -translate-x-1/2 bg-gradient-to-r from-purple-400 to-violet-600 
+      text-white border-none text-sm  font-light py-2 px-20 rounded-full cursor-pointer'>  
+      <button>
+      Logout
+      </button>
+      </div>
+    
     </div>
+    
   )
 }
 
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import assets from '../assets/assets'
 
 const LoginPage = () => {
+  const[currState,setCurrstate] = useState("sign-up")
+  const[fullName,setFullname] = useState("")
+  const[Email,setEmail] = useState("")
+  const[Bio,setBio] = useState("")
+  const[Password,setPassword] = useState("")
+  const[isDataSubmitted,setisDataSubmitted] = useState("")
+
   return (
-    <div>LoginPage</div>
+    <div className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8
+    backdrop-blur-2xl sm:justify-evenly max-sm:flex-col '>
+      {/* ---left---- */}
+      <img src={assets.logo_big} alt='' className='max-w-50'/>
+      {/* ----right---- */}
+      <form className='border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg'>
+       <h2 className='flex'>{currState}
+        <img src={assets.arrow_icon} className='w-5 cursor-pointer'/>
+       </h2>
+       {currState === "sign-up" && !isDataSubmitted && (
+        <input onClick={(e)=>{setFullname(e.target.value)}} value={fullName} type='text' className='p-2 border border-gray-500 rounded-md focus:outline-none
+        'placeholder='Full Name' required/>
+       )}
+       {!isDataSubmitted && 
+        <>
+        <input onClick={(e)=>{setEmail(e.target.value)}} value={Email} type='email'placeholder='Email' required className='p-2 border border-gray-500 rounded-md focus:outline-none'/>
+        <input onClick={(e)=>{setPassword(e.target.value)}} value={Password} type='password'placeholder='Password' required className='p-2 border border-gray-500 rounded-md focus:outline-none'/>
+        </>
+       }
+      </form>
+
+    </div>
   )
 }
 
